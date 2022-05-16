a7d04906e130f02d724d5190bb8ddf3be222db81
aukic <anteukic@hotmail.com>
2022-05-16 19:58:53 +0200

    Lv4

--- src/App.js
+++ src/App.js
@@ -1,10 +1,55 @@
+import logo from "./logo.svg";
 import "./App.css";
-import Cards from "./components/Cards";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Popup from "./components/Popup";
+import {
+  Container,
+  Row,
+  Col,
+  Button,
+  Breadcrumb,
+  Card,
+  Form,
+  Nav,
+  Navbar,
+  NavDropdown,
+} from "react-bootstrap";
+import ListCards from "./components/Cards";
+import { Route, Router, Routes } from "react-router-dom";
+import Details from "./components/Details";
 
 function App() {
   return (
     <div className="App">
-      <Cards />
+      <header className="App-header">
+        <Navbar
+          bg="dark"
+          variant="dark"
+          sticky="top"
+          expand="sm"
+          collapseOnSelect
+        ></Navbar>
+        <Container>
+          <Navbar
+            bg="dark"
+            variant="dark"
+            sticky="top"
+            expand="sm"
+            collapseOnSelect
+            className="mt-3"
+          >
+            <Navbar.Toggle className="coloring" />
+            <Navbar.Collapse>
+              <Nav className="d-flex flex-row justify-content-center">
+                <Nav.Link href="#cars">Cars</Nav.Link>
+                <Nav.Link href="#motorcycles">Motorcycles</Nav.Link>
+                <Nav.Link href="#vans">Vans</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
+          <ListCards />
+        </Container>
+      </header>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,40 +1,67 @@
 import React from "react";
-import PropTypes from "prop-types";
 import { Button } from "react-bootstrap";
-
+import { useState } from "react";
 import "./card.css";
+import audi from "../assets/audi.jpg";
+import Popup from "./Popup";
 
-function Card({ imageSource, title, text, url }) {
+function Card({
+  name,
+  year,
+  acceleration,
+  cylinders,
+  displacement,
+  horespower,
+  milesPerGalon,
+  origin,
+  weight,
+  price,
+  mileage,
+}) {
+  const [buttonPopup, setButtonPopup] = useState(false);
   return (
     <div className="card text-center bg-dark animate__animated animate__fadeInUp">
       <div className="overflow">
-        <img src={imageSource} alt="a wallpaper" className="card-img-top" />
+        {<img src={audi} alt="a wallpaper" className="card-img-top" />}
       </div>
       <div className="card-body text-light">
-        <h4 className="card-title">{title}</h4>
-        <p className="card-text text-secondary">
-          {text
-            ? text
-            : "Lorema askfhjasfjaspdj apasjdjaspdjkpasd pasjdppasdop "}
-        </p>
-    
+        <h4 className="card-title">{name}</h4>
+        <div className="m-2 text-start">
+          <span>Year: </span>
+          {year}
+        </div>
+        <div className="m-2 text-start">
+          <span>Price: {price} $</span>
+        </div>
+        <div className="m-2 text-start">
+          <span>Milage: {mileage} km</span>
+        </div>
         <div className="d-flex flex-row mb-3 justify-content-center">
-        <div className="p-2" > <Button variant="primary">BUY</Button></div>
-        <div className="p-2"> <Button  variant="primary">INFO</Button></div>
-      </div>
-      
-
-        
+          <div className="p-2">
+            {" "}
+            <Button variant="primary" onClick={() => setButtonPopup(true)}>
+              Details
+            </Button>
+            <Popup
+              trigger={buttonPopup}
+              setTrigger={setButtonPopup}
+              name={name}
+              year={year}
+              acceleration={acceleration}
+              cylinders={cylinders}
+              displacement={displacement}
+              horespower={horespower}
+              milesPerGalon={milesPerGalon}
+              origin={origin}
+              weight={weight}
+              price={price}
+              mileage={mileage}
+            ></Popup>
+          </div>
+        </div>
       </div>
     </div>
   );
 }
-
-Card.propTypes = {
-  title: PropTypes.string.isRequired,
-  text: PropTypes.string,
-  url: PropTypes.string,
-  imageSource: PropTypes.string
-};
 
 export default Card;
